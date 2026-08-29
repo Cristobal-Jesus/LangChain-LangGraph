0b1544a085644b2176a8483d868bd3a0999e843b
CristobalUll <alu0101755601@ull.edu.es>
2026-08-29 13:11:51 +0100

    Searching with Tavily's tools

--- src/langchain_langgraph/main.py
+++ src/langchain_langgraph/main.py
@@ -3,31 +3,14 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 from typing import Any, cast
 
 
 load_dotenv()
 
-tavily = TavilyClient()
-
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-
-    Args:
-        query (str): The query to search for
-
-    Returns:
-        str: The search results
-    """
-    print(f"Searching for: {query}")
-    return tavily.search(query=query)
-
 llm = ChatOllama(model="qwen3:4b", temperature=0) 
-tools = [search]
+tools = [TavilySearch()]
 agent: Any = cast(Any, create_agent(model=llm, tools=tools))
 
 def main() -> None:
