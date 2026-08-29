2585947b6c9ca3ff44924c0684811effefe7a6eb
CristobalUll <alu0101755601@ull.edu.es>
2026-08-29 13:08:56 +0100

    First seaching with custom tool using Tavily

--- src/langchain_langgraph/main.py
+++ src/langchain_langgraph/main.py
@@ -2,13 +2,38 @@ from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from tavily import TavilyClient
+from typing import Any, cast
 
 
 load_dotenv()
 
+tavily = TavilyClient()
+
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+
+    Args:
+        query (str): The query to search for
+
+    Returns:
+        str: The search results
+    """
+    print(f"Searching for: {query}")
+    return tavily.search(query=query)
+
+llm = ChatOllama(model="qwen3:4b", temperature=0) 
+tools = [search]
+agent: Any = cast(Any, create_agent(model=llm, tools=tools))
+
 def main() -> None:
     print("Hello from langchain-langgraph!")
+    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Tokyo?")]}) 
+    print(result) 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": 
+    main() 
